86df0f81e6a88e1d6db140d66bbc96b6021a870d
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-11-20 04:17:28 +0100

    setup firebase, auth and emulator

--- parking_app_flutter/client_user/lib/main.dart
+++ parking_app_flutter/client_user/lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  if (kDebugMode) {
+    await FirebaseAuth.instance.useAuthEmulator("localhost", 9099);
+  }
+
   runApp(const MainApp());
 }
