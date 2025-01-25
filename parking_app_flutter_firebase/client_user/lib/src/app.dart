512734e11414c91a1d0e8f32c3a07fc33e4e2f2d
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-25 04:39:10 +0100

    move AuthIsUserSignedIn event to main.dart and convert MainApp to stateless

--- parking_app_flutter_firebase/client_user/lib/src/app.dart
+++ parking_app_flutter_firebase/client_user/lib/src/app.dart
@@ -5,27 +5,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/constants/constants.dart';
 import 'core/routing/routing.dart';
 import 'core/theme/theme.dart';
-import 'features/auth/bloc/auth_bloc.dart';
 import 'features/settings/state/dark_mode_cubit.dart';
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  @override
-  void initState() {
-    super.initState();
-    context.read<AuthBloc>().add(AuthIsUserSignedIn());
-  }
 
   @override
   Widget build(BuildContext context) {
     final darkModeCubit = context.watch<DarkModeCubit>();
-
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: AppConstants.appName,
