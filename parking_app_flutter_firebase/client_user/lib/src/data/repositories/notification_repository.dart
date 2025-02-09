958b60bad35007590eeb31b4429721f396744f06
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-02-09 23:53:43 +0100

    implement NotificationRepository to handle local notifications

---
+++ parking_app_flutter_firebase/client_user/lib/src/data/repositories/notification_repository.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+import '../../../local_notifications.dart';
+
+class NotificationRepository {
+  final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      flutterLocalNotificationsPlugin;
+
+  Future<void> scheduleParkingReminder({
+    required int id,
+    required DateTime endTime,
+  }) async {
+    await requestPermissions();
+
+    // TODO: set timer to 10 minutes before endTime
+    // final notificationTime = endTime.subtract(const Duration(minutes: 10));
+    final notificationTime = endTime.subtract(const Duration(seconds: 10));
+
+    // Ensure the notification is in the future
+    if (notificationTime.isAfter(DateTime.now())) {
+      await _notificationsPlugin.zonedSchedule(
+        id,
+        "Parkheim Reminder",
+        "Your parking session will end in 10 minutes.",
+        tz.TZDateTime.from(notificationTime, tz.local),
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            "parking_reminders",
+            "Parking Reminders",
+            channelDescription: "Notifications for parking session reminders",
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: "ticker",
+          ),
+          iOS: DarwinNotificationDetails(),
+        ),
+        androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+      );
+    } else {
+      debugPrint(
+        "❌ -> Notification time is in the past. Notification not scheduled.",
+      );
+    }
+  }
+
+  Future<void> rescheduleParkingReminder(int id, DateTime newEndTime) async {
+    // Cancel existing notification
+    await cancelParkingReminder(id);
+
+    // Schedule new notification with updated endTime
+    await scheduleParkingReminder(id: id, endTime: newEndTime);
+  }
+
+  Future<void> cancelParkingReminder(int id) async {
+    await _notificationsPlugin.cancel(id);
+  }
+}
